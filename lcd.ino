c156d5077408013ca7cd2215f4f4059b87051d82
MythicalCow <62105787+MythicalCow@users.noreply.github.com>
2023-06-10 13:34:58 -0400

    LED matrix, buzzer, and lcd integrated on RTOS

---
+++ lcd.ino
@@ -0,0 +1,16 @@
+// #include <LiquidCrystal.h>
+
+// LiquidCrystal lcd(7, 8, 13, 12, 11, 10);
+
+// void setup() {
+//   lcd.begin(16, 2);
+//   lcd.print("Hello, World!");
+// }
+
+// void loop() {
+//   // set the cursor to column 0, line 1
+//   // (note: line 1 is the second row, since counting begins with 0):
+//   lcd.setCursor(0, 1);
+//   // print the number of seconds since reset:
+//   lcd.print(millis() / 1000);
+// }
